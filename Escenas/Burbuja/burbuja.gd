d4479f2cc24d4b6f1a02bdb17fae840c11fbd925
kevinMG01 <montanokevin46@gmail.com>
2025-01-20 21:24:48 -0300

    s

--- Escenas/Burbuja/burbuja.gd
+++ Escenas/Burbuja/burbuja.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-var roca
+var roca = preload("res://Escenas/objetos/roca.tscn")
 var madera
 var piedra
 
@@ -11,7 +11,7 @@ var objetosDeProteccion = {
 	"madera": 0,
 	"piedra": 0
 }
-
+var cantidadObjetos = objetosDeProteccion.roca + objetosDeProteccion.madera + objetosDeProteccion.piedra
 var habilidad = 0
 
 const SPEED = 550
@@ -40,35 +40,10 @@ func recolectarProtector(item : String):
 	if item == "roca":
 		objetosDeProteccion.roca += 1
 
-var objetosInstanciados = 0
+func spawnDefenza():
+	if cantidadObjetos >=3:
+		pass
 
-func _ready():
-	# Instanciar aleatoriamente 3 objetos de protección cuando inicia el juego
-	while objetosInstanciados < 3:
-		var objetoAleatorio = obtener_objeto_aleatorio()
-		if objetoAleatorio:
-			var instancia = objetoAleatorio.instance()
-			# Ajustar posición aleatoria dentro de la escena
-			instancia.position = Vector2(randf_range(100, 800), randf_range(100, 600))
-			add_child(instancia)
-			objetosInstanciados += 1
-
-func obtener_objeto_aleatorio() -> PackedScene:
-	# Lista de objetos disponibles
-	var objetosDisponibles = []
-	
-	if objetosDeProteccion["roca"] == 0:
-		objetosDisponibles.append(roca)
-	if objetosDeProteccion["madera"] == 0:
-		objetosDisponibles.append(madera)
-	if objetosDeProteccion["piedra"] == 0:
-		objetosDisponibles.append(piedra)
-	
-	# Elegir aleatoriamente un objeto de los disponibles
-	if objetosDisponibles.size() > 0:
-		return objetosDisponibles[randi() % objetosDisponibles.size()]
-	
-	return null
 
 
 func _on_detector_body_entered(body):
