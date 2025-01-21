117eb1c9d4743aac31ea4e9363c3ec48e0e75409
kevinMG01 <montanokevin46@gmail.com>
2025-01-21 17:17:31 -0300

    s

--- Escenas/Niveles/nivel_1.gd
+++ Escenas/Niveles/nivel_1.gd
@@ -3,16 +3,31 @@ extends Node2D
 
 var roca = preload("res://Escenas/objetos/roca.tscn")
 var cuchilloCoc = preload("res://Escenas/objetos/cuchillo_coc.tscn")
+var agujas = preload("res://Escenas/objetos/agujas.tscn")
+var cuchillo = preload("res://Escenas/objetos/cuchillo.tscn")
 
 @onready var marker1 = $instanObj/Izq
 @onready var marker2 = $instanObj/Der
 
 var cantidadObjetos = 2
 
+var tiempo = 45
+
+
 func _ready():
 	randomize()
 	$tiempoSpawn.wait_time = 2
 	$tiempoSpawn.start()
+	$temporizador.wait_time =tiempo
+	$temporizador.start()
+	
+
+
+func _physics_process(delta):
+
+	$textTiempo.text = "Tiempo: " + str(int($temporizador.time_left))
+	pass
+
 
 func spawn(obj):
 	for i in range(cantidadObjetos):
@@ -34,9 +49,9 @@ func _on_tiempo_spawn_timeout():
 	if newspawn == 2:
 		spawn(cuchilloCoc)
 	if newspawn == 3:
-		spawn(roca)
+		spawn(agujas)
 	if newspawn == 4:
-		spawn(roca)
+		spawn(cuchillo)
 	var num = randi_range(0,2)
 	cantidadObjetos = num
 	var tim = randf_range(0.5, 1.3)
@@ -46,7 +61,7 @@ func _on_tiempo_spawn_timeout():
 
 func _on_viento_timeout():
 	GlovalVar.viento = true
-	var v = randi_range(1, 2)
+	var v = randi_range(1, 3)
 	if v == 1:
 		GlovalVar.vientoIzqDer = true
 	elif v == 2:
@@ -60,3 +75,7 @@ func _on_detener_viento_timeout():
 	GlovalVar.viento = false
 	$detenerViento.stop()
 
+func _on_temporizador_timeout():
+	get_tree().paused = true
+	
+	pass
---
+++ Escenas/objetos/cuchillo.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+
+var item = "cuchillo"
+var viento = true
+
+#const SPEED = 200.0
+var gravity = 240
+
+
+
+func _ready():
+	grupos()
+
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta 
+
+	rotation_degrees += 1
+	#if velocity.length() > 0:
+		#velocity = velocity.normalized() * SPEED
+	if viento == true:
+		if GlovalVar.viento == true:
+			if GlovalVar.vientoIzqDer == true:
+				velocity.x += 3
+			if GlovalVar.vientoIzqDer == false:
+				velocity.x -= 3
+
+	move_and_slide()
+
+func removerGrupo():
+	remove_from_group("enemigo")
+	remove_from_group("objetos")
+
+func asignarGrupo(grup: String):
+	add_to_group(grup)
+
+var grupo_anterior: String = ""  # Variable para almacenar el valor anterior
+
+func _process(delta):
+	if GlovalVar.grupo != grupo_anterior:  # Compara el valor actual con el anterior
+		grupos()
+		grupo_anterior = GlovalVar.grupo  # Actualiza el valor anterior para la próxima comparación
+	
+func grupos():
+	if GlovalVar.grupo == "enemigo":
+		removerGrupo()
+		asignarGrupo("enemigo")
+	if GlovalVar.grupo == "objetos":
+		removerGrupo()
+		asignarGrupo("objetos")
