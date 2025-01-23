01fb214425ea3d6cbc481d09b8738943df4e489b
kevinMG01 <montanokevin46@gmail.com>
2025-01-23 12:21:33 -0300

    g

--- Escenas/Burbuja/burbuja.gd
+++ Escenas/Burbuja/burbuja.gd
@@ -41,11 +41,24 @@ func _physics_process(delta):
 			$ContHabi.start(4)
 			habilidad -= 1
 
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+
+	# Movimiento en el eje X
+	if direction_x:
+		velocity.x = direction_x * SPEED
+		if direction_x == 1:
+			$NuevoLienzo.flip_h = true
+		elif direction_x == -1:
+			$NuevoLienzo.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Movimiento en el eje Y
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 	move_and_slide()
 
