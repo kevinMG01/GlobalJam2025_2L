a8aa68c2f6d27f8bf216a72381ac104409dea87f
DAGimenez <gimenezdylan753@gmail.com>
2025-01-23 13:03:13 -0300

    2
    se creo escena de creditos y se le dio funcionalidad a los 3 botones:D

--- Escenas/Menu/menu.gd
+++ Escenas/Menu/menu.gd
@@ -27,3 +27,16 @@ func _on_tim_play_timeout():
 
 func _on_creditos_button_down():
 	$play.play()
+
+#Funciones Del Menu :
+ #Jugar
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/Niveles/nivel_1.tscn")
+
+#Creditos
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Creditos/Creditos.tscn")
+
+#Salir
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
